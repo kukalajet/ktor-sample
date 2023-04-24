5c33b55642346a1576358fa019b87165125f41e3
Jeton Kukalaj <jeton.kukalaj@gmail.com>
2023-04-24 09:02:44 +0200

    initial commit

---
+++ src/main/kotlin/xyz/kukalajet/Application.kt
@@ -0,0 +1,15 @@
+package xyz.kukalajet
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import xyz.kukalajet.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+            .start(wait = true)
+}
+
+fun Application.module() {
+    configureRouting()
+}
---
+++ src/test/kotlin/xyz/kukalajet/ApplicationTest.kt
@@ -0,0 +1,5 @@
+package xyz.kukalajet
+
+
+class ApplicationTest {
+}
